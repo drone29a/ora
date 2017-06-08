9eecb93f3dd9656342c3e56405fdb76fc8422c3f
Matt Revelle <mrevelle@gmail.com>
2017-06-07 21:38:09 -0400

    Some basic code to get mic line.

--- src/ora/core.clj
+++ src/ora/core.clj
@@ -1,5 +1,32 @@
 (ns ora.core
+  (:import [javax.sound.sampled
+            TargetDataLine
+            DataLine
+            DataLine$Info
+            AudioFormat
+            AudioSystem])
   (:gen-class))
+
+(set! *warn-on-reflection* true)
+
+(defn create-audio-format
+  ^AudioFormat
+  []
+  (AudioFormat. 44100
+                16
+                1
+                true
+                false))
+
+(defn create-data-line-info
+  ^DataLine$Info
+  [audio-format]
+  (DataLine$Info. TargetDataLine audio-format))
+
+(defn get-target-data-line
+  ^TargetDataLine
+  [^DataLine$Info info]
+  (AudioSystem/getLine info))
 
 (defn -main
   "I don't do a whole lot ... yet."
