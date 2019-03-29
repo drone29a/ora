276e59aee5de656331c58741ad3d72e3a1f0f2c4
Matt Revelle <mrevelle@gmail.com>
2019-03-29 14:19:32 -0600

    Update deps.

--- project.clj
+++ project.clj
@@ -3,12 +3,10 @@
   :url "http://github.com/mattrepl/ora"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/core.async "0.4.474"]
-                 ;;[tarsos-dsp/tarsos-dsp "2.4"]
-                 [ddf/minim "2.2.2"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/core.async "0.4.490"]
                  [com.github.wendykierp/JTransforms "3.1"]
-                 [quil "2.7.1"]]
+                 [quil "2.8.0"]]
   :main ^:skip-aot ora.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
